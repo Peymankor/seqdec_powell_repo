60ac0e57e9bef9070cbb500dd13332bcddc52c4d
Peymankor <Peymankor@github.com>
2021-01-09 17:07:01 +0100

    adding files of chapter 2

---
+++ Chap2_Assett_Selling/function/test.py
@@ -0,0 +1,13 @@
+import os
+pathold = os.getcwd()
+print(pathold) 
+
+os.chdir("/home/peyman/Documents/PhD_UiS/optimum-techinvest/powell_seqdec/stochastic-optimization/AssetSelling")
+pathnew=os.getcwd()
+print(pathnew)
+import numpy as np
+help(np.linspace)
+np.linspace(0.0,0.0)
+theta_low_values1 = np.linspace(0.5, 1)
+theta_low_values1
+theta_high_values1 = np.linspace(high_min, high_max)
